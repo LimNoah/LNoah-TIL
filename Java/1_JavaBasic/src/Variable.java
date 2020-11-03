c056b357d4c1437df3097c477223d8dbbea5d19f
LNoah <limnoah0301@gmail.com>
2020-11-04 00:18:22 +0900

    Java TIL

---
+++ Java/1_JavaBasic/src/Variable.java
@@ -0,0 +1,39 @@
+public class Variable {
+
+    /**
+     * 변수 (Variable)
+     * 변하는 수. 수학의 변수와 유사
+     * 변수의 타입, 변수명, 값 할당
+     * 변수의 선언
+     * 변수에 값 할당 (리터럴)
+     * 변수명 규칙
+     */
+
+    public static void main(String[] args) {
+        int x;
+        x = 10; // 10 -> 리터럴
+
+        System.out.println(x);
+
+        int y, z, value;
+
+        int valueOne = 10;
+        int valueTwo = 20;
+
+        int valueThree = 100, valueFour = 1000;
+
+        // int int;
+        //int 4thWord;
+        int val2ue1;
+
+        int 한글완전가능;
+
+        //int fwei&wfe;
+        int $power; // 특수문자 사용 가능하나 권장하지 않음
+
+        // Code Convention
+        int nvalueOfComputer;        // 헝가리안 표기법
+        int valueOfComputer;         // 카멜 표기법
+        int ValueOfComputer;         // 파스칼 표기법법
+    }
+}
